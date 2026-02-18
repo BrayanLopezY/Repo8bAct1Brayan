5b947f833cf60d6cdd9dd7f3cea119e804352021
BrayanLopezY <lopezgomezbrayanbersain@gmail.com>
2026-02-17 19:25:56 -0600

    Refactor code structure for improved acerca_de

---
+++ acerca_de.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de BuraianPage's</title>
+</head>
+
+<body>
+
+    <h1>Acerca de BuraianPage's</h1>
+
+    <p>
+        Hola, este es un texto informativo sobre Brayan.
+        Esto es para una practica con git hub con ramas,
+        aprenderemos a trabajar de mejor manera en git.
+    </p>
+
+    <img src="imagen/foto.png" alt="dude" width="300">
+
+    <br><br>
+
+    <a href="index.php">
+        < Volver al inicio</a>
+
+</body>
+
+</html>
